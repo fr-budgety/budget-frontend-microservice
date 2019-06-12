d3a60025c325f0aed99359fb15f770b8c015aef4
Filippo Rivolta <rivoltafilippo@gmail.com>
2019-06-12 16:57:56 +0200

    Adding list

--- src/modules/Expenses/pages/ExpenseDashboard.js
+++ src/modules/Expenses/pages/ExpenseDashboard.js
@@ -3,15 +3,27 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { clearErrors } from "../../../redux/actions/accountActions";
 import { setCurrentPage } from "../../../redux/actions/layoutActions";
+import {getCategories, deleteCategory, toggleAddCategoryModal, toggleEditCategoryModal} from '../../../redux/actions/categoryActions';
 import { getIcons } from '../../../redux/actions/iconActions';
 import DashboardLayout from "../../../layouts/DashboardLayout/DashboardLayout";
 import MainContentArea from "../../../components/grid/MainContentArea";
 import ExpenseActions from "../modules/ExpenseActions";
 import ExpenseList from "../modules/ExpenseList";
+import Confirmation from "../../../components/confirmation/Confirmation";
+import Title from "../../../components/typography/Title";
+
 
 
 class ExpenseDashboard extends Component {
-  componentDidMount(){
+  constructor(props) {
+		super(props);
+		this.state = {
+      deleteConfirmationIsClosed: false,
+      addIsActive: false
+		};
+  }
+
+  componentDidMount() {
     this.props.setCurrentPage('expenses');
     this.props.getIcons();
   }
@@ -19,14 +31,42 @@ class ExpenseDashboard extends Component {
   sortExpenses = (expensesToSort) => {
     return expensesToSort;
   }
-  
+
+  //Change state and display modal for delete confirmation
+	handleToggleConfirmation = () => {
+		this.setState({
+			deleteConfirmationIsClosed: !this.state.deleteConfirmationIsClosed
+		});
+  };
+
+  //Handle modal add category activation from state
+  handleAddCategoryActivation = ()=>{
+    const {addCategoryModalIsOpen} = this.props.categories
+    const action = !addCategoryModalIsOpen
+    this.props.toggleAddCategoryModal(action);
+    this.props.clearErrors();
+  }
+
   render() {
     return (
       <DashboardLayout title="Expenses">
-          <MainContentArea>
-            <ExpenseActions/>
-            <ExpenseList expenses={this.sortExpenses(this.props.expenses)}/>
-          </MainContentArea>      
+        <MainContentArea>
+          <ExpenseActions />
+          <ExpenseList
+            expenses={this.sortExpenses(this.props.expenses)}
+            handleToggleConfirmation={this.handleToggleConfirmation}
+            />
+        </MainContentArea>
+        <Confirmation
+          visible={this.state.deleteConfirmationIsClosed}
+          onConfirmationModalClose={this.handleToggleConfirmation}
+          handleConfirmationCallback={this.handleDeleteCategory}
+        >
+          <Title variant="h2" color="alt">
+            Are you sure you want to delete this category?
+					</Title>
+          <p className="darkColorAlt">You will delete this category and all related expenses.</p>
+        </Confirmation>
       </DashboardLayout>
     );
   }
@@ -49,5 +89,5 @@ const mapStateToProps = state => {
 };
 export default connect(
   mapStateToProps,
-  { setCurrentPage, clearErrors, getIcons }
+  { setCurrentPage, clearErrors, getIcons, toggleAddCategoryModal }
 )(ExpenseDashboard);
